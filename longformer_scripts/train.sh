e265adb3f891bf6d09bb27ec2086d9da5a0d9e46
christine <christine.raouf88@gmail.com>
2021-06-07 15:02:47 +0200

    #doc representations if the valid or test tokens exceeds 3000 we do not add this document

--- longformer_scripts/train.sh
+++ longformer_scripts/train.sh
@@ -25,8 +25,8 @@ mkdir -p $CP_DIR
 
 #arch should be replaced by transformer_big instead of tiny
 #adding two parameters for the longf
-stdbuf -i0 -e0 -o0 fairseq-train $WORKING_DIR \
-  --arch  transformer_tiny  --optimizer adam --adam-betas '(0.9, 0.98)' \
+stdbuf -i0 -e0 -o0 fairseq-train $WORKING_DIR --task translation_longformer \
+  --user-dir ../fairseq/tasks --arch  transformer_tiny  --optimizer adam --adam-betas '(0.9, 0.98)' \
   --clip-norm 0.0 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 \
   --warmup-updates 4000 --lr 0.001 --min-lr 1e-09 --dropout 0.1 \
   --weight-decay 0.0 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
