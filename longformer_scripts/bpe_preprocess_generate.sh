f2ac47490153750f7d76cd5cbd50b767a936864a
christine <christine.raouf88@gmail.com>
2021-10-01 16:42:14 +0200

    # BPE generate file

---
+++ longformer_scripts/bpe_preprocess_generate.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+#SBATCH -p veu # Partition to submit to
+#SBATCH --gres=gpu:0
+#SBATCH --mem=20G # Memory
+#SBATCH --ignore-pbs
+#SBATCH --output=preprocess-detok.log
+echo 'Cloning Subword NMT repository (for BPE pre-processing)...'
+git clone https://github.com/rsennrich/subword-nmt.git
+
+
+SRC="en"
+TGT="de"
+
+GENERATE_PREF="generate.tok.tc"
+MODEL_DIR="/home/usuaris/scratch/christine.raouf.saad/news_v11/NCv11_en-es/lftransfomer_model"  #MODEL
+DEST_DIR="/home/usuaris/scratch/christine.raouf.saad/google_dataset/data"
+TRN_PREF="train.tok.tc"
+
+
+N_OP=1000
+
+BPEROOT='../subword-nmt/subword_nmt'
+#we do not learn...we apply, we learn only
+echo "apply bpe to " $SRC
+
+echo "learn_bpe.py on ${TRAIN}..."
+
+
+#$BPEROOT/learn_bpe.py -s $N_OP < ${WORKING_DIR}/${TRN_PREF}.${SRC} > ${DEST_DIR}/${TRN_PREF}.codes.${SRC}
+$BPEROOT/apply_bpe.py -c  ${MODEL_DIR}/${TRN_PREF}.codes.${SRC} < ${DEST_DIR}/${GENERATE_PREF}.${SRC} >  ${DEST_DIR}/${GENERATE_PREF}.bpe.${SRC}
+
+
+echo "apply bpe to " $TGT
+#$BPEROOT/learn_bpe.py -s $N_OP < ${WORKING_DIR}/${TRN_PREF}.${TGT} > ${DEST_DIR}/${TRN_PREF}.codes.${TGT}
+$BPEROOT/apply_bpe.py -c  ${MODEL_DIR}/${TRN_PREF}.codes.${SRC} < ${DEST_DIR}/${GENERATE_PREF}.${TGT} >  ${DEST_DIR}/${GENERATE_PREF}.bpe.${TGT}
+
