045660941a01b31adb7e568e0601a1dc6e4e0de5
christine <christine.raouf88@gmail.com>
2021-10-11 14:51:31 +0200

    # skip connection change layer number

--- fairseq/models/transformer_lf_skip_connection.py
+++ fairseq/models/transformer_lf_skip_connection.py
@@ -41,8 +41,8 @@ DEFAULT_MAX_TARGET_POSITIONS = 1024
 DEFAULT_MIN_PARAMS_TO_WRAP = int(1e8)
 
 
-@register_model("transformer_skip_connection")
-class TransformerSkipConnectionModel(FairseqEncoderDecoderModel):
+@register_model("transformer_lf_skip_connection")
+class TransformerLFSkipConnectionModel(FairseqEncoderDecoderModel):
     """
     Transformer model from `"Attention Is All You Need" (Vaswani, et al, 2017)
     <https://arxiv.org/abs/1706.03762>`_.
@@ -102,6 +102,8 @@ class TransformerSkipConnectionModel(FairseqEncoderDecoderModel):
             'transformer.wmt20.ta-en': spm('https://dl.fbaipublicfiles.com/fairseq/models/wmt20.ta-en.single.tar.gz'),
             'transformer.wmt20.iu-en.news': spm('https://dl.fbaipublicfiles.com/fairseq/models/wmt20.iu-en.news.single.tar.gz'),
             'transformer.wmt20.iu-en.nh': spm('https://dl.fbaipublicfiles.com/fairseq/models/wmt20.iu-en.nh.single.tar.gz'),
+            'transformer.flores101.mm100.615M': spm('https://dl.fbaipublicfiles.com/flores101/pretrained_models/flores101_mm100_615M.tar.gz'),
+            'transformer.flores101.mm100.175M': spm('https://dl.fbaipublicfiles.com/flores101/pretrained_models/flores101_mm100_175M.tar.gz'),
         }
         # fmt: on
 
@@ -196,6 +198,9 @@ class TransformerSkipConnectionModel(FairseqEncoderDecoderModel):
                             help='block size of quantization noise at training time')
         parser.add_argument('--quant-noise-scalar', type=float, metavar='D', default=0,
                             help='scalar quantization noise and scalar quantization at training time')
+
+        parser.add_argument('--lf-rep-dim', type=float, metavar='D', default=768,
+                            help='dimension of longformer representations')
         # args for Fully Sharded Data Parallel (FSDP) training
         parser.add_argument(
             '--min-params-to-wrap', type=int, metavar='D', default=DEFAULT_MIN_PARAMS_TO_WRAP,
@@ -209,6 +214,10 @@ class TransformerSkipConnectionModel(FairseqEncoderDecoderModel):
             )
         )
         # fmt: on
+
+        #todo: the size of the longformer
+        #the size of the embedding
+        #put it in tiny transformer or send it in train.sh
 
     @classmethod
     def build_model(cls, args, task):
@@ -299,6 +308,9 @@ class TransformerSkipConnectionModel(FairseqEncoderDecoderModel):
         src_tokens,
         src_lengths,
         prev_output_tokens,
+        lf_reps: Optional = None,
+        doc_exist_ids: Optional = None,
+        after_pad_tokens_ids: Optional = None,
         return_all_hiddens: bool = True,
         features_only: bool = False,
         alignment_layer: Optional[int] = None,
@@ -311,7 +323,8 @@ class TransformerSkipConnectionModel(FairseqEncoderDecoderModel):
         which are not supported by TorchScript.
         """
         encoder_out = self.encoder(
-            src_tokens, src_lengths=src_lengths, return_all_hiddens=return_all_hiddens
+            src_tokens, src_lengths=src_lengths, return_all_hiddens=return_all_hiddens, lf_reps=lf_reps,
+            doc_exist_ids= doc_exist_ids, after_pad_tokens_ids=after_pad_tokens_ids
         )
         decoder_out = self.decoder(
             prev_output_tokens,
@@ -399,31 +412,22 @@ class TransformerEncoder(FairseqEncoder):
         self.layers.extend(
             [self.build_encoder_layer(args) for i in range(args.encoder_layers)]
         )
-        #skip connection in the middle
-        layer_skip_connection=self.build_encoder_layer_skip_connection(args)
+        layer_skip_connection = self.build_encoder_layer_skip_connection(args)
         self.num_layers = len(self.layers)
-        layer_to_replace = math.ceil(self.num_layers/2)
-        if(args.after_skip and args.two_skip):
-            self.layers[layer_to_replace] = layer_skip_connection
-            self.layers[layer_to_replace + 1] = layer_skip_connection
-        elif(args.before_skip and args.two_skip):
-            self.layers[layer_to_replace] = layer_skip_connection
-            self.layers[layer_to_replace - 1] = layer_skip_connection
-        elif(args.mid_skip):
-            self.layers[layer_to_replace] = layer_skip_connection
-        elif (args.after_skip):
-            self.layers[layer_to_replace + 1] = layer_skip_connection
-        elif(args.before_skip):
-            self.layers[layer_to_replace - 1] = layer_skip_connection
-
-
-
+        layer_to_replace = math.ceil(self.num_layers / 2)
+        self.layers[layer_to_replace] = layer_skip_connection
 
 
         if args.encoder_normalize_before:
             self.layer_norm = LayerNorm(embed_dim)
         else:
             self.layer_norm = None
+
+        #segment embeddings
+        self.lf_rep_dim=args.lf_rep_dim
+        print('lf_rep_dim in transformer class:')
+        print(self.lf_rep_dim)
+        self.lin_lf = nn.Linear(self.lf_rep_dim, embed_dim)
 
     def build_encoder_layer(self, args):
         layer = TransformerEncoderLayer(args)
@@ -456,12 +460,43 @@ class TransformerEncoder(FairseqEncoder):
         return layer
 
     def forward_embedding(
-        self, src_tokens, token_embedding: Optional[torch.Tensor] = None
+        self, src_tokens, token_embedding: Optional[torch.Tensor] = None, lf_reps: Optional = None,
+        doc_exist_ids: Optional = None,
+        after_pad_tokens_ids: Optional = None,
     ):
+
+
         # embed tokens and positions
         if token_embedding is None:
             token_embedding = self.embed_tokens(src_tokens)
+
         x = embed = self.embed_scale * token_embedding
+        # TODO(Christine) lf_reps in the first <doc> token!
+        #linear?
+        # from long_former embedding to word embeddings
+        print('longformer reps shape:')
+        #print(lf_reps.shape)
+        print('x shape:')
+        #print(x.shape)
+
+        # added 11 Aug...can be removed
+        if(doc_exist_ids is not None):
+            if(torch.numel(doc_exist_ids)):
+                if(lf_reps is not None):
+                    lf_reps_after_trans = self.lin_lf(lf_reps)
+                    print('longformer reps after trans:')
+                    print(lf_reps_after_trans.shape)
+                #x[mask_ids, 0,:] for certain ids to be replaced
+                #x[mask_ids,pad,:] for certain ids with the token after left pad
+                    x[doc_exist_ids,after_pad_tokens_ids,:] = lf_reps_after_trans
+                    print('after change:')
+                    print('x shape:')
+                    print(x.shape)
+                    print( x[doc_exist_ids,after_pad_tokens_ids,:])
+
+
+
+        #x[:, 0] = lf_reps_after_trans
         if self.embed_positions is not None:
             x = embed + self.embed_positions(src_tokens)
         if self.layernorm_embedding is not None:
@@ -475,6 +510,9 @@ class TransformerEncoder(FairseqEncoder):
         self,
         src_tokens,
         src_lengths: Optional[torch.Tensor] = None,
+        lf_reps:Optional= None,
+        doc_exist_ids: Optional = None,
+        after_pad_tokens_ids: Optional = None,
         return_all_hiddens: bool = False,
         token_embeddings: Optional[torch.Tensor] = None,
     ):
@@ -503,6 +541,9 @@ class TransformerEncoder(FairseqEncoder):
         """
         return self.forward_scriptable(src_tokens,
                                        src_lengths,
+                                       lf_reps,
+                                       doc_exist_ids,
+                                       after_pad_tokens_ids,
                                        return_all_hiddens,
                                        token_embeddings)
 
@@ -514,6 +555,9 @@ class TransformerEncoder(FairseqEncoder):
         self,
         src_tokens,
         src_lengths: Optional[torch.Tensor] = None,
+        lf_reps: Optional= None,
+        doc_exist_ids: Optional= None,
+        after_pad_tokens_ids: Optional= None,
         return_all_hiddens: bool = False,
         token_embeddings: Optional[torch.Tensor] = None,
     ):
@@ -544,7 +588,7 @@ class TransformerEncoder(FairseqEncoder):
         encoder_padding_mask = src_tokens.eq(self.padding_idx)
         has_pads = (src_tokens.device.type == "xla" or encoder_padding_mask.any())
 
-        x, encoder_embedding = self.forward_embedding(src_tokens, token_embeddings)
+        x, encoder_embedding = self.forward_embedding(src_tokens, token_embeddings, lf_reps, doc_exist_ids, after_pad_tokens_ids)
 
         # account for padding while computing the representation
         if has_pads:
@@ -1096,7 +1140,7 @@ def Linear(in_features, out_features, bias=True):
     return m
 
 
-@register_model_architecture("transformer_skip_connection", "transformer_skip_connection_tiny")
+@register_model_architecture("transformer_lf_skip_connection", "transformer_lf_skip_connection_tiny")
 def tiny_architecture(args):
     args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 64)
     args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 64)
@@ -1104,10 +1148,12 @@ def tiny_architecture(args):
     args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 2)
     args.decoder_layers = getattr(args, "decoder_layers", 2)
     args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 2)
-    return base_architecture(args)
+    #todo(christine) define the lf_rep_dim
+    args.lf_rep_dim=getattr(args, "lf-rep-dim", 768)
+    base_architecture(args)
 
 
-@register_model_architecture("transformer_skip_connection", "transformer_skip_connection")
+@register_model_architecture("transformer_lf_skip_connection", "transformer_lf_skip_connection")
 def base_architecture(args):
     args.encoder_embed_path = getattr(args, "encoder_embed_path", None)
     args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 512)
@@ -1163,7 +1209,7 @@ def base_architecture(args):
     args.quant_noise_scalar = getattr(args, "quant_noise_scalar", 0)
 
 
-@register_model_architecture("transformer_skip_connection", "transformer_skip_connection_iwslt_de_en")
+@register_model_architecture("transformer_lf_skip_connection", "transformer_lf_skip_connection_iwslt_de_en")
 def transformer_iwslt_de_en(args):
     args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 512)
     args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 1024)
@@ -1173,20 +1219,17 @@ def transformer_iwslt_de_en(args):
     args.decoder_ffn_embed_dim = getattr(args, "decoder_ffn_embed_dim", 1024)
     args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 4)
     args.decoder_layers = getattr(args, "decoder_layers", 6)
-    args.after_skip = getattr(args, "after_skip", True)
-    args.two_skip = getattr(args, "two_skip", True)
-    args.before_skip = getattr(args, "before_skip", True)
-    args.mid_skip = getattr(args, "mid_skip", True)
+    args.lf_rep_dim=getattr(args, "lf-rep-dim", 768)
     base_architecture(args)
 
 
-@register_model_architecture("transformer_skip_connection", "transformer_skip_connection_wmt_en_de")
+@register_model_architecture("transformer_lf_skip_connection", "transformer_lf_skip_connection_wmt_en_de")
 def transformer_wmt_en_de(args):
     base_architecture(args)
 
 
 # parameters used in the "Attention Is All You Need" paper (Vaswani et al., 2017)
-@register_model_architecture("transformer_skip_connection", "transformer_skip_connection_vaswani_wmt_en_de_big")
+@register_model_architecture("transformer_lf_skip_connection", "transformer_lf_skip_connection_vaswani_wmt_en_de_big")
 def transformer_vaswani_wmt_en_de_big(args):
     args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 1024)
     args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 4096)
@@ -1196,23 +1239,25 @@ def transformer_vaswani_wmt_en_de_big(args):
     args.decoder_ffn_embed_dim = getattr(args, "decoder_ffn_embed_dim", 4096)
     args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 16)
     args.dropout = getattr(args, "dropout", 0.3)
+    # todo(christine) defining the dimension of longformer
+    args.lf_rep_dim = getattr(args, "lf-rep-dim", 768)
     base_architecture(args)
 
 
-@register_model_architecture("transformer_skip_connection", "transformer_skip_connection_vaswani_wmt_en_fr_big")
+@register_model_architecture("transformer_lf_skip_connection", "transformer_lf_skip_connection_vaswani_wmt_en_fr_big")
 def transformer_vaswani_wmt_en_fr_big(args):
     args.dropout = getattr(args, "dropout", 0.1)
     transformer_vaswani_wmt_en_de_big(args)
 
 
-@register_model_architecture("transformer_skip_connection", "transformer_skip_connection_wmt_en_de_big")
+@register_model_architecture("transformer_lf_skip_connection", "transformer_lf_skip_connection_wmt_en_de_big")
 def transformer_wmt_en_de_big(args):
     args.attention_dropout = getattr(args, "attention_dropout", 0.1)
     transformer_vaswani_wmt_en_de_big(args)
 
 
 # default parameters used in tensor2tensor implementation
-@register_model_architecture("transformer_skip_connection", "transformer_skip_connection_wmt_en_de_big_t2t")
+@register_model_architecture("transformer_lf_skip_connection", "transformer_lf_skip_connection_wmt_en_de_big_t2t")
 def transformer_wmt_en_de_big_t2t(args):
     args.encoder_normalize_before = getattr(args, "encoder_normalize_before", True)
     args.decoder_normalize_before = getattr(args, "decoder_normalize_before", True)
